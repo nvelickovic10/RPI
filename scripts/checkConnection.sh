c4b5cc479fb0c56f786c7ec47bb94fe0be786bf2
Nemanja Velickovic <nemanja.velickovic@livenation.com>
2018-03-16 13:46:44 +0100

    added new script

--- scripts/checkConnection.sh
+++ scripts/checkConnection.sh
@@ -17,7 +17,6 @@ CURRENT_IP_ADDRESS=$(curl -s ipinfo.io/ip)
 if [ ! -f  ${PUBLIC_IP_ADDRESS_FILE} ]; then
     logInfo "First time check"
     PUBLIC_IP_ADDRESS=""
-    STATUS=2
 else
     PUBLIC_IP_ADDRESS=$(cat ${PUBLIC_IP_ADDRESS_FILE})
 fi
@@ -25,16 +24,13 @@ fi
 logInfo "OLD: $PUBLIC_IP_ADDRESS"
 logInfo "NEW: $CURRENT_IP_ADDRESS"
 
-STATUS=0
 if [[ ${CURRENT_IP_ADDRESS} == ${PUBLIC_IP_ADDRESS} ]]; then
     logInfo "IP address did not change"
 else
     logInfo "IP address changed!!"
-    AA=${CURRENT_IP_ADDRESS}
     echo ${CURRENT_IP_ADDRESS} > ${PUBLIC_IP_ADDRESS_FILE}
     mpack -s "Internet connection checker" ${PUBLIC_IP_ADDRESS_FILE} nvelickovic10@gmail.com
 fi
 
 logInfo "FINISHED ${SCRIPT_NAME}"
-exit ${STATUS}
 #END
---
+++ scripts/checkForVideoCamera.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# Change dir to script dir
+cd "$(dirname "$0")"
+
+# Include common functions
+. common.sh
+
+SCRIPT_NAME="${0##*/}"
+
+logInfo "STARTING ${SCRIPT_NAME}"
+
+function startMotionService {
+    if [[ "${MOTION_STATUS}" == "inactive" ]]; then
+        logInfo "Starting motion"
+        service motion start
+    else
+        logInfo "Motion already started"
+    fi
+}
+
+function stopMotionService {
+    if [[ "${MOTION_STATUS}" == "active" ]]; then
+        logInfo "Stopping motion"
+        service motion stop
+    else
+        logInfo "Motion already stopped"
+    fi
+}
+
+CAMERA_DEVICE=/dev/video0
+MOTION_STATUS=$(systemctl is-active motion)
+
+logInfo "Motion status: ${MOTION_STATUS}"
+
+if ls -ltrh ${CAMERA_DEVICE} ; then
+    startMotionService
+else
+    stopMotionService
+fi
+
+logInfo "FINISHED ${SCRIPT_NAME}"
+# END
